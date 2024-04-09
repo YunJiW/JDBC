b108158c1b113f7c9cc92a1e3166931b71b88d30
YunJiW <dtroy371@gmail.com>
2024-04-09 16:42:05 +0900

    log : log check

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,9 +1,11 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -30,7 +32,22 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
-    private void useDataSource(DriverManagerDataSource dataSource) throws SQLException {
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(ConnectionConst.URL);
+        dataSource.setUsername(ConnectionConst.USERNAME);
+        dataSource.setPassword(ConnectionConst.PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
 
         Connection con1 = dataSource.getConnection();
         Connection con2 = dataSource.getConnection();
