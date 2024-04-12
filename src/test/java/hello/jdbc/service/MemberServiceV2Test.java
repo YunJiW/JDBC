47db9b3de90452df63fcdd647cb95dabd82469e0
YunJiW <dtroy371@gmail.com>
2024-04-12 14:08:56 +0900

    Test : Transaction accountTransferEx

--- src/test/java/hello/jdbc/service/MemberServiceV2Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV2Test.java
@@ -2,7 +2,6 @@ package hello.jdbc.service;
 
 import hello.jdbc.connection.repository.MemberRepositoryV2;
 import hello.jdbc.domain.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +11,8 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import java.sql.SQLException;
 
 import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MemberServiceV2Test {
 
@@ -48,8 +49,32 @@ class MemberServiceV2Test {
         Member findMemberA = memberRepository.findById(memberA.getMemberId());
         Member findMemberB = memberRepository.findById(memberB.getMemberId());
 
-        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(8000);
-        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(12000);
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체중 예외 발생")
+    void accountTransferEx() throws SQLException {
+
+        Member memberA = new Member("memberA", 10000);
+        Member memberEx = new Member("ex", 10000);
+
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+
+        assertThatThrownBy(() ->
+                memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberEx = memberRepository.findById(memberEx.getMemberId());
+
+        assertThat(findMemberA.getMoney()).isEqualTo(10000);
+        assertThat(findMemberEx.getMoney()).isEqualTo(10000);
+
     }
 
 }
