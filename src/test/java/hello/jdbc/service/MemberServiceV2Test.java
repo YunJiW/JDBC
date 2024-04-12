2dafc245e6d09ff201c21680e47b8b3a6b26048e
YunJiW <dtroy371@gmail.com>
2024-04-12 14:05:11 +0900

    Test : accointTransfer

---
+++ src/test/java/hello/jdbc/service/MemberServiceV2Test.java
@@ -0,0 +1,55 @@
+package hello.jdbc.service;
+
+import hello.jdbc.connection.repository.MemberRepositoryV2;
+import hello.jdbc.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+class MemberServiceV2Test {
+
+    private MemberRepositoryV2 memberRepository;
+    private MemberServiceV2 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV2(dataSource);
+        memberService = new MemberServiceV2(dataSource, memberRepository);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete("memberA");
+        memberRepository.delete("memberB");
+        memberRepository.delete("ex");
+    }
+
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+
+        Member memberA = new Member("memberA", 10000);
+        Member memberB = new Member("memberB", 10000);
+
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+}
